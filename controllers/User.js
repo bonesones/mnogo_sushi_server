9f98cb484f0a6c97bd5ff3146842f7ee5326a469
Dmitriy <dmitriyworkspace013@gmail.com>
2024-06-17 15:57:06 +0300

    test

--- controllers/User.js
+++ controllers/User.js
@@ -15,7 +15,7 @@ const createJWT = (id, email, role) => {
 
 class User {
     async signup(req, res, next) {
-        const { email, phone, password, submit_password, role='USER', name = '' } = req.body
+        const { email, phone, password, submit_password, role='ADMIN', name = '' } = req.body
         try {
             const isEmailBusy = await UserModel.isUserExistByField("email", email);
             const isPhoneBusy = await UserModel.isUserExistByField('phone', phone)
